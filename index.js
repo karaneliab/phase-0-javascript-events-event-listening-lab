dbc855cb8a6407abc4a3ace9450be0210429c229
karaneliab <eliabkaran7884@gmail.com>
2023-12-22 12:32:50 +0300

    tried to put the click alert fuction under add event listener function and tried to style it

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
 function addingEventListener() {
+    const input = document.getElementById('input');
+input.addEventListener('click', clickAlert);
+  alert('I was clicked!');
+  
+
 }
+function clickAlert (){
+    alert('I was clicked!');
+   
+    input.style.color = 'blue';
+}
+addEventListener('click', clickAlert);
+// const input = document.getElementById('input');
+// function clickAlert (){
+//     alert('I was clicked!');
+   
+//     this.style.color = 'blue';
+// }
+
+// input.addEventListener('click', clickAlert)
